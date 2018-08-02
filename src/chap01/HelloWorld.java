9a149d2db5b3886be36dc4c5a8dace2ae3d40dd6
Satoru Tanaka <ittimfn@gmail.com>
2018-08-02 20:24:19 +0900

    AtomicIntegerを使用する。

--- src/chap01/HelloWorld.java
+++ src/chap01/HelloWorld.java
@@ -1,13 +1,25 @@
 package chap01;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class HelloWorld {
 
 	public static void main(String[] args){
-		ThreadedHello hello = new ThreadedHello(5);
 
+		// ThreadHello
+		ThreadedHello hello = new ThreadedHello(5);
+		threadStart(hello);
+
+		// AtomicThreadHello
+		AtomicThreadHello atHello = new AtomicThreadHello(5);
+		threadStart(atHello);
+	}
+
+	private static void threadStart(Runnable r){
+		System.out.println("----- " + r.getClass() + " -----");
 		Thread[] thread = new Thread[3];
 		for(int i=0;i<3;i++){
-			thread[i] = new Thread(hello);
+			thread[i] = new Thread(r);
 		}
 		for(int i=0;i<3;i++){
 			thread[i].start();
@@ -18,7 +30,10 @@ public class HelloWorld {
 			}
 		}catch (InterruptedException ex){
 			ex.printStackTrace();
+		}finally{
+			System.out.println("----- finish -----");
 		}
+
 	}
 
 }
@@ -44,6 +59,31 @@ class ThreadedHello implements Runnable{
 			}
 
 			System.out.printf("%s:%d\n",Thread.currentThread().getName(),counter);
+		}
+	}
+}
+
+class AtomicThreadHello implements Runnable{
+	private AtomicInteger counter;
+
+	public AtomicThreadHello(int counter){
+		this.counter = new AtomicInteger(counter);
+	}
+
+	@Override
+	public void run() {
+		for(int i=0;i<3;i++){
+			try{
+				Thread.sleep(50);
+			}catch(InterruptedException ex){
+				ex.printStackTrace();
+			}
+
+			synchronized (this) {
+				counter.getAndIncrement();
+			}
+
+			System.out.printf("%s:%d\n",Thread.currentThread().getName(),counter.intValue());
 		}
 	}
 }
